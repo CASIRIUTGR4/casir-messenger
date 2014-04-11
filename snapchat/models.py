7c75c9fff00b46cde7175fd88a640a3075d39c81
Gaetan Chagneau <chagneag@pc-a104-15.iut-valence.fr>
2014-04-11 12:15:08 +0200

    modification bdd

--- snapchat/models.py
+++ snapchat/models.py
@@ -1,22 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 #Create your models here.
-class User(models.Model):
-    id = models.IntegerField(primary_key=True)
-    login = models.CharField(max_length=25)
-    password = models.CharField(max_length=20)
-    mail = models.EmailField(unique=True)
        
 class Message(models.Model):
     id = models.IntegerField(primary_key=True)
-    sender = models.ForeignKey('User', related_name='sender')
-    recever = models.ForeignKey('User', related_name='recever')
+    sender = models.ForeignKey(User, related_name='sender')
+    recever = models.ForeignKey(User, related_name='recever')
     message = models.CharField(max_length=100)
     attachment = models.ImageField(upload_to='photos')
     duration = models.TimeField()
     created = models.DateField()
        
 class Friend(models.Model):
-    idDemande = models.ForeignKey('User', related_name='demande')
-    idFriend = models.ForeignKey('User', related_name='friend')
+    idDemande = models.ForeignKey(User, related_name='demande')
+    idFriend = models.ForeignKey(User, related_name='friend')
     invitation = models.BooleanField()
