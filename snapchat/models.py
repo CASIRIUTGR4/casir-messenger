27e4d28b4f68a53d5ca53263d82bc482bcfdb293
Loic Chaufournais <loic.chaufournais@hotmail.fr>
2014-04-11 10:13:41 +0200

    Correction model py Generation base de donnée

--- snapchat/models.py
+++ snapchat/models.py
@@ -6,18 +6,18 @@ class User(models.Model):
     id = models.IntegerField(primary_key=True)
     login = models.CharField(max_length=25)
     password = models.CharField(max_length=20)
-    mail = models.EmailField()
+    mail = models.EmailField(unique=True)
       
 class Message(models.Model):
     id = models.IntegerField(primary_key=True)
-    sender = models.ForeignKey('User')
-    recever = models.ForeignKey('User')
+    sender = models.ForeignKey('User', related_name='sender')
+    recever = models.ForeignKey('User', related_name='recever')
     message = models.CharField(max_length=100)
-    attachment = models.ImageField()
+    attachment = models.ImageField(upload_to='photos')
     duration = models.TimeField()
     created = models.DateField()
       
 class Friend(models.Model):
-    idDemande = models.ForeignKey('User')
-    idFriend = models.ForeignKey('User')
+    idDemande = models.ForeignKey('User', related_name='demande')
+    idFriend = models.ForeignKey('User', related_name='friend')
     invitation = models.BooleanField()
