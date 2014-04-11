e9740f31bb92d497760a2df8710f6ce03670cb4f
Romain Gueffier <gueffier@pc-a104-13.iut-valence.fr>
2014-04-11 10:40:47 +0200

    Première gestion de connexion

--- snapchat/views.py
+++ snapchat/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
 def index(request):
-    
-    context = {
-               "message": "Page d'accueil et d'authentification",
-               "connexion":"Zone de connexion",
-               "inscription": "Zone d'inscription",
-               }
-    
-    return render(request, "snapchat/index.html", context)
+    if request.POST:
+        username = request.POST['login']
+        password = request.POST['password']
+        user = authenticate(username=login, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'snapchat/dashboard.html')
+            else:
+                context = {
+                           "disable_account": "Ce compte est désactivé !"
+                           }
+        else:
+            context = {
+                       "wrong_logins": "Vos identifiants sont erronés !"
+                       }
+        
+        context = {
+                   "message": "Page d'accueil et d'authentification",
+                   "connexion":"Zone de connexion",
+                   "inscription": "Zone d'inscription",
+                   }
+        
+        return render(request, "snapchat/index.html", context)
 
 def dashboard(request):
     
