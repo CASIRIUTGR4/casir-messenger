792cbfbc851f5c47813acb997abf38cf8d4f7568
Loic Chaufournais <loic.chaufournais@hotmail.fr>
2014-04-17 14:37:53 +0200

    - Création d'une page de presentation - Corection mineur dans fichier base.html - Ajout redirection vers page de presentation

--- casir_messenger/settings_dev.py
+++ casir_messenger/settings_dev.py
@@ -1,6 +1,3 @@
 DEBUG = True
-<<<<<<< HEAD
 TEMPLATE_DEBUG = True
-=======
 MODE_DEBUG = True
->>>>>>> 2b4a9f84fc7cb5ce781a48865d7d37765d3f0639
--- snapchat/models.py
+++ snapchat/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-<<<<<<< HEAD
-=======
 
 #Create your models here.
->>>>>>> 2b4a9f84fc7cb5ce781a48865d7d37765d3f0639
        
 class Message(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -18,8 +15,4 @@ class Message(models.Model):
 class Friend(models.Model):
     idDemande = models.ForeignKey(User, related_name='demande')
     idFriend = models.ForeignKey(User, related_name='friend')
-<<<<<<< HEAD
     invitation = models.BooleanField()
-=======
-    invitation = models.BooleanField()
->>>>>>> 2b4a9f84fc7cb5ce781a48865d7d37765d3f0639
